14cd357d0c654d21da46931992291a26c7bc9a05
Devpjyt <moon1323m@gmail.com>
2022-11-26 10:08:27 +0530

    change in cp.js file

--- src/getstarted/Createpassword.js
+++ src/getstarted/Createpassword.js
@@ -16,11 +16,11 @@ export const CreatePassword = () => {
         var url = document.URL;
         var id = url.substring(url.lastIndexOf('?') + 1);
         alert(id);
-        axios
-        .get('https://entity-feediiapi.azurewebsites.net/api/login/getverifyToken/' + id)
-        .then((res) => {
-            alert('working');
-        })
+        // axios
+        // .get('https://entity-feediiapi.azurewebsites.net/api/login/getverifyToken/' + id)
+        // .then((res) => {
+        //     alert('working');
+        // })
     })
 
     // React.useEffect(
