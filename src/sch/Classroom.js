2f16b48a863095301c273eb31f1ceff5c7eaed8a
Devpjyt <moon1323m@gmail.com>
2022-12-05 10:23:01 +0530

    classroom page update

--- src/sch/Classroom.js
+++ src/sch/Classroom.js
@@ -27,65 +27,57 @@ export const ClassroomPage = () => {
                                     <div className="clsrmdv2 col-sm-12 row">
                                         <div className="col-sm-4 mb-4">
                                             <Link to="" className="tlbxdvvda">
-                                                <img src="https://res.cloudinary.com/infoi/image/upload/q_auto/v1646637617/Dashboard/New%20courses%20images/final_images/ux_ui_design_foundations.svg" alt="Class Image" className="clsrmdv2-1-bg-img1" />
                                                 <div className="clsrmdv2-1">
-                                                    <div className="clsrmdv2-2-dv1">
+                                                    <div className="clsrmdv2-2-dv1 mb-0">
                                                         <div className="clsrmdv2-2-dv1-a1">Class 1st</div>
+                                                        <div class="clsrmdv2-2-dv3-dv">A</div>
                                                         <div className="clsrmdv2-2-dv1-a2">
                                                             <i className="fa fa-ellipsis-v"></i>
                                                         </div>
                                                     </div>
-                                                    <div className="clsrmdv2-2-dv3">
-                                                        <div className="clsrmdv2-2-dv3-dv">Section A</div>
-                                                    </div>
+                                                    <img src="https://res.cloudinary.com/infoi/image/upload/q_auto/v1646637617/Dashboard/New%20courses%20images/final_images/ux_ui_design_foundations.svg" alt="Class Image" className="clsrmdv2-1-bg-img1" />
                                                 </div>
                                             </Link>
                                         </div>
                                         <div className="col-sm-4 mb-4">
                                             <Link to="" className="tlbxdvvda">
-                                                <img src="https://res.cloudinary.com/infoi/image/upload/q_auto/v1646637615/Dashboard/New%20courses%20images/final_images/design_terminology.svg" alt="Class Image" className="clsrmdv2-1-bg-img1" />
                                                 <div className="clsrmdv2-1">
-                                                    <div className="clsrmdv2-2-dv1">
+                                                    <div className="clsrmdv2-2-dv1 mb-0">
                                                         <div className="clsrmdv2-2-dv1-a1">Class 1st</div>
+                                                        <div class="clsrmdv2-2-dv3-dv">B</div>
                                                         <div className="clsrmdv2-2-dv1-a2">
                                                             <i className="fa fa-ellipsis-v"></i>
                                                         </div>
                                                     </div>
-                                                    <div className="clsrmdv2-2-dv3">
-                                                        <div className="clsrmdv2-2-dv3-dv">Section B</div>
-                                                    </div>
+                                                    <img src="https://res.cloudinary.com/infoi/image/upload/q_auto/v1646637615/Dashboard/New%20courses%20images/final_images/design_terminology.svg" alt="Class Image" className="clsrmdv2-1-bg-img1" />
                                                 </div>
                                             </Link>
                                         </div>
                                         <div className="col-sm-4 mb-4">
                                             <Link to="" className="tlbxdvvda">
-                                                <img src="https://res.cloudinary.com/infoi/image/upload/q_auto/v1646637614/Dashboard/New%20courses%20images/final_images/color_psychology.svg " alt="Class Image" className="clsrmdv2-1-bg-img1" />
                                                 <div className="clsrmdv2-1">
-                                                    <div className="clsrmdv2-2-dv1">
+                                                    <div className="clsrmdv2-2-dv1 mb-0">
                                                         <div className="clsrmdv2-2-dv1-a1">Class 1st</div>
+                                                        <div class="clsrmdv2-2-dv3-dv">C</div>
                                                         <div className="clsrmdv2-2-dv1-a2">
                                                             <i className="fa fa-ellipsis-v"></i>
                                                         </div>
                                                     </div>
-                                                    <div className="clsrmdv2-2-dv3">
-                                                        <div className="clsrmdv2-2-dv3-dv">Section C</div>
-                                                    </div>
+                                                    <img src="https://res.cloudinary.com/infoi/image/upload/q_auto/v1646637614/Dashboard/New%20courses%20images/final_images/color_psychology.svg " alt="Class Image" className="clsrmdv2-1-bg-img1" />
                                                 </div>
                                             </Link>
                                         </div>
                                         <div className="col-sm-4 mb-4">
                                             <Link to="" className="tlbxdvvda">
-                                                <img src="https://res.cloudinary.com/infoi/image/upload/q_auto/v1646637614/Dashboard/New%20courses%20images/final_images/design_accessibility.svg" alt="Class Image" className="clsrmdv2-1-bg-img1" />
                                                 <div className="clsrmdv2-1">
-                                                    <div className="clsrmdv2-2-dv1">
+                                                    <div className="clsrmdv2-2-dv1 mb-0">
                                                         <div className="clsrmdv2-2-dv1-a1">Class 1st</div>
+                                                        <div class="clsrmdv2-2-dv3-dv">D</div>
                                                         <div className="clsrmdv2-2-dv1-a2">
                                                             <i className="fa fa-ellipsis-v"></i>
                                                         </div>
                                                     </div>
-                                                    <div className="clsrmdv2-2-dv3">
-                                                        <div className="clsrmdv2-2-dv3-dv">Section D</div>
-                                                    </div>
+                                                    <img src="https://res.cloudinary.com/infoi/image/upload/q_auto/v1646637614/Dashboard/New%20courses%20images/final_images/design_accessibility.svg" alt="Class Image" className="clsrmdv2-1-bg-img1" />
                                                 </div>
                                             </Link>
                                         </div>
