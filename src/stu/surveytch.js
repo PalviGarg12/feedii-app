1737c5e65486d78e0d44aa2aafe09ed958cf3df4
Devpjyt <moon1323m@gmail.com>
2023-01-03 12:15:17 +0530

    remove alert from student surveytch page

--- src/stu/surveytch.js
+++ src/stu/surveytch.js
@@ -48,9 +48,7 @@ export const SurveyTeacherStudentPage = () => {
 
 
     React.useEffect(
-        ()=> {      
-
-    alert(sessionstudentid + " | " + sessionpulseid);
+        ()=> {
 
 
         fetch('https://entity-feediiapi.azurewebsites.net/api/Student/getStudentTeacherSurveydetail/' + sessionstudentid + "-" + sessionpulseid, {
